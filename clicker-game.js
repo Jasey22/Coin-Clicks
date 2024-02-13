83c9721da5ff1007c2389be593243bb24385548a
Jasey22 <106456744+Jasey22@users.noreply.github.com>
2024-02-14 12:26:04 +1300

    Add files via upload

---
+++ clicker-game.js
@@ -0,0 +1,50 @@
+let coins = 0;
+
+function btnCoin() {
+    coins++;
+    document.getElementById("totalCoins").innerHTML = coins;
+}
+
+let autoClick = 0;
+let autoClickUpgrade = 10;
+
+function btnAutoClicker() {
+    if (coins >= autoClickUpgrade) {
+        autoClick++;
+        coins = coins - autoClickUpgrade;
+
+        autoClickUpgrade = Math.round(autoClickUpgrade * 1.1);
+        document.getElementById("totalCoins").innerHTML = coins;
+        document.getElementById("totalAutoClicker").innerHTML = autoClick;
+        document.getElementById("clickerCost").innerHTML = autoClickUpgrade;
+        coinsPerSecond();
+    }
+}
+
+let golddigger = 0;
+let diggerUpgrade = 20;
+
+function btnDigger() {
+    if (coins >= diggerUpgrade) {
+        golddigger++;
+        coins = coins - diggerUpgrade;
+
+        diggerUpgrade = Math.round(diggerUpgrade * 1.2);
+        document.getElementById("totalCoins").innerHTML = coins;
+        document.getElementById("totalDigger").innerHTML = golddigger;
+        document.getElementById("diggerCost").innerHTML = diggerUpgrade;
+        coinsPerSecond();
+    }
+}
+
+function coinsPerSecond() {
+    cps = autoClick * 1 + golddigger * 2;
+    document.getElementById("cps").innerHTML = cps;
+}
+
+setInterval(function () {
+    coins = coins + autoClick * 1 + golddigger * 2;
+    document.getElementById("totalCoins").innerHTML = coins;
+    coinsPerSecond();
+    
+}, 1000);
